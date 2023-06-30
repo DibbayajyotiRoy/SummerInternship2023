dd5699ad33d1d8dc49ccbc9f6230f5f0646fe828
Dibbayajyoti <125145390+DibbayajyotiRoy@users.noreply.github.com>
2023-06-30 21:17:24 +0530

    Commit 30

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,4 +47,58 @@ window.addEventListener("scroll", scrollHeader);
 
 /*=============== CALCULATE JS ===============*/
 
+const calculateForm = document.getElementById("calculate-form"),
+  calculateCm = document.getElementById("calculate-cm"),
+  calculateKg = document.getElementById("calculate-kg"),
+  calculateMessage = document.getElementById("calculate-message");
+
+const calculateBmi = (e) => {
+  e.preventDefault();
+  /*Check if the fields have a value*/
+
+  if (calculateCm.value === "" || calculateKg.value === "") {
+    //Add and remove color
+    calculateMessage.classList.remove("color-green");
+    calculateMessage.classList.add("color-red");
+
+    //Show message
+    calculateMessage.textContent = "Fill in the Height and Weight🧑🏻‍💻";
+
+    //Remove message Three seconds
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 3000);
+  } else {
+    //BMI Formula
+    const cm = calculateCm.value / 100;
+    const kg = calculateKg.value;
+    const bmi = Math.round(kg / (cm * cm));
+
+    //Show health status
+    if (bmi < 18.5) {
+      //Add color and display message
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny 😣`;
+    } else if (bmi < 25) {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are healthy 😃`;
+    } else {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are overweight😣`;
+    }
+
+    //To clear the Input value
+
+    calculateCm.value = "";
+    calculateKg.value = "";
+
+    //Remove message four seconds
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 4000);
+  }
+};
+
+calculateForm.addEventListener("submit", calculateBmi);
+
 /*=============== EMAIL JS ===============*/
